9ac06e03803c49e8c329a22ab61b7d01094c96fb
delsint123 <delsint123@gmail.com>
2022-08-09 21:13:03 -0400

    Add operate and roundDecimals functions

--- javascript.js
+++ javascript.js
@@ -4,50 +4,72 @@
 
 // const buttons = document.querySelector('.buttons');
 
-//set as object for later use
-function add(object) {
-    let result = object.value1 + object.value2;
+//set as operand for later use
+function add(operands) {
+    let result = operand.value1 + operand.value2;
 
     //save result if further calculations need to be made
-    object.value1 = result;
-    object.value2 = 0;
+    operand.value1 = result;
+    operand.value2 = 0;
     
     return result;
-}
+};
 
-function subtract(object) {
-    let result = object.value1 - object.value2;
+function subtract(operands) {
+    let result = operand.value1 - operand.value2;
 
     //save result if further calculations need to be made
-    object.value1 = result;
-    object.value2 = 0;
+    operand.value1 = result;
+    operand.value2 = 0;
     
     return result;
-}
+};
 
-function multiply(object) {
-    let result = object.value1 * object.value2;
+function multiply(operands) {
+    let result = operand.value1 * operand.value2;
 
     //save result if further calculations need to be made
-    object.value1 = result;
-    object.value2 = 0;
+    operand.value1 = result;
+    operand.value2 = 0;
     
     return result;
-}
+};
 
-function divide(object) {
-    let result = object.value1 / object.value2;
+function divide(operand) {
+    let result = operand.value1 / operand.value2;
 
     //save result if further calculations need to be made
-    object.value1 = result;
-    object.value2 = 0;
+    operand.value1 = result;
+    operand.value2 = 0;
+
+    result = roundDecimals(result);
     
     return result;
-}
+};
+
+function roundDecimals(result) {
+    if(Math.floor(result) === result) return result;
+    return parseFloat(result.toFixed(6)); 
+};
+
+function operate(operand, operator) {
+    if(operator == '+') {
+        add(operand);
+    }
+    else if(operator == '-') {
+        subtract(operand);
+    }
+    else if(operator == '*') {
+        multiply(operand);
+    }
+    else if(operator == '/') {
+        divide(operand);
+    }
+};
 
 //testing purposes
-let object = {
-    value1: 4,
-    value2: 10,
-}
-console.log(divide(object));
+let operand = {
+    value1: 10,
+    value2: 3,
+};
+console.log(divide(operand));
